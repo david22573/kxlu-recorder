28e1db626f9e4b296165ac3febd8c3eb4b8e75d9
david22573 <davidmiguel22573@gmail.com>
2023-05-13 04:10:30 -0700

    Reset main to run schedule

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from jobs import run_scheduler
-from upload_file import main
 
-main()
-# upload_file('./music/main_test_05_13_2023.mp3', 'kxlu-music')
-# record_playlist('main_test', 15 * 60)
-
-# run_scheduler()
+run_scheduler()
