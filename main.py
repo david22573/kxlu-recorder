b148cb0fcb7d30c8ae8074c605de03cb56ccb7ae
david22573 <davidmiguel22573@gmail.com>
2024-01-24 18:20:56 -0800

    fix: main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kxlu_recorder.jobs import DJSetScheduler
 
 
 def main():
-    dj_set_scheduler = DJSetScheduler.run_scheduler()
+    DJSetScheduler.run_scheduler()
 
 
 if __name__ == "__main__":
